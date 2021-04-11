9d34da9d1bbb856a11c7374ca969697757fc0208
James Chang <jc4095@nyu.edu>
2021-04-10 20:31:31 -0400

    Adjust privacy page spacing

--- frontend/pages/legal/privacy.vue
+++ frontend/pages/legal/privacy.vue
@@ -10,26 +10,54 @@
             </p>
             <p>
               CubePB.com is a website for tracking and showcasing your personal
-              bests in speedcubing events. The website source code is available under GPL-3.0 open source license, and it can be viewed at the <a href="https://github.com/cubing/CubePB" target="_blank"
+              bests in speedcubing events. The website source code is available
+              under GPL-3.0 open source license, and it can be viewed at the
+              <a href="https://github.com/cubing/CubePB" target="_blank"
                 >Github repository</a
               >.
             </p>
-            <p>
+            <div class="pb-2">
               <span class="subheader">Data</span>
               <ul>
-                <li>When you login, CubePB stores your name, email, profile picture, country of origin, and WCA ID, as recorded on your WCA account. You can manage your WCA account settings <a @click="goToWcaAccountManagement()">here</a>.</li>
-                <li>When you enter PBs (Personal Bests), CubePB stores your PB data under the current logged in user.</li>
-                <li>By default, your profile page, with your user information and PBs will be made publicly visible for everyone to see. However, you can adjust this behavior in the <nuxt-link to="/settings">settings page</nuxt-link>.</li>
-                <li>When you login, a temporary cookie is stored to identify you to the website, even after you close the browser session. When you logout, or too much time has passed, the cookie is automatically deleted.</li>
+                <li>
+                  When you login, CubePB stores your name, email, profile
+                  picture, country of origin, and WCA ID, as recorded on your
+                  WCA account. You can manage your WCA account settings
+                  <a @click="goToWcaAccountManagement()">here</a>.
+                </li>
+                <li>
+                  When you enter PBs (Personal Bests), CubePB stores your PB
+                  data under the current logged in user.
+                </li>
+                <li>
+                  By default, your profile page, with your user information and
+                  PBs will be made publicly visible for everyone to see.
+                  However, you can adjust this behavior in the
+                  <nuxt-link to="/settings">settings page</nuxt-link>.
+                </li>
+                <li>
+                  When you login, a temporary cookie is stored to identify you
+                  to the website, even after you close the browser session. When
+                  you logout, or too much time has passed, the cookie is
+                  automatically deleted.
+                </li>
               </ul>
-            </p>
-            <p>
+            </div>
+            <div class="pb-2">
               <span class="subheader">Security</span>
               <ul>
-                <li>This website works strictly over HTTPS to provide you a secured environment.</li>
-                <li>Your email address is not accessible by other users on the site. However, it can be viewed by the site administrator, for administration purposes. We promise not to share your email with others.</li>
+                <li>
+                  This website works strictly over HTTPS to provide you a
+                  secured environment.
+                </li>
+                <li>
+                  Your email address is not accessible by other users on the
+                  site. However, it can be viewed by the site administrator, for
+                  administration purposes. We promise not to share your email
+                  with others.
+                </li>
               </ul>
-            </p>
+            </div>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
@@ -47,10 +75,8 @@
 <script>
 import { goToWcaAccountManagement } from '~/services/auth'
 export default {
-
   methods: {
-    goToWcaAccountManagement
-  }
-
+    goToWcaAccountManagement,
+  },
 }
 </script>
